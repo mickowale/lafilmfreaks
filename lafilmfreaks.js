ddd0105540c6945d91d14398ca0be234eee0e5b5
Michael Tegegn <michaelwalelegn11@gmail.com>
2020-06-09 00:45:28 +0900

    Use relative path for files

--- lafilmfreaks.js
+++ lafilmfreaks.js
@@ -4,4 +4,22 @@ $( document ).ready(function() {
   // We do most of the necassary javascript here.
 
 
+  /* For the movie page */
+
+  function getProgressBarClass (value) {
+    if (value <= 3) return "green";
+    else if (value <= 7) return "orange";
+    else return "red";
+  }
+  $('.progressBar').click(function (e) {
+    var x = e.pageX - this.offsetLeft, // or e.offsetX (less support, though)
+        y = e.pageY - this.offsetTop,  // or e.offsetY
+        clickedValue = x * this.max / this.offsetWidth,
+        adjusted_value = Math.round (clickedValue)
+  this.value = adjusted_value;	
+  $(this).removeClass()
+  $(this).addClass (getProgressBarClass(this.value));
+    // this.style.color = "blue";
+});
+
 });
